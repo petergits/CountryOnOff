a03b8872df1212f65bbefdc0f0b7b540645f4e92
Peter Gits <pgits@geekgaps.com>
2019-09-11 16:10:15 -0500

    Initial Commit

---
+++ CountryOnOff/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CountryOnOff
+//
+//  Created by Peter Adient Gits on 9/11/19.
+//  Copyright © 2019 GeekGaps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CountryOnOffTests/CountryOnOffTests.swift
@@ -0,0 +1,36 @@
+//
+//  CountryOnOffTests.swift
+//  CountryOnOffTests
+//
+//  Created by Peter Adient Gits on 9/11/19.
+//  Copyright © 2019 GeekGaps. All rights reserved.
+//
+
+import XCTest
+@testable import CountryOnOff
+
+class CountryOnOffTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
